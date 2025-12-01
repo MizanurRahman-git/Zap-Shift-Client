bc8104cb4dd3077b0700c392f27bf414ab896d34
MizanurRahmanHridoy <mizanurrahmanhridoy42@gmail.com>
2025-12-02 00:58:14 +0600

    add rechart function

--- src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
 
 const AdminDashboardHome = () => {
   const axiosSecure = useAxiosSecure();
@@ -11,6 +12,14 @@ const AdminDashboardHome = () => {
       return res.data;
     },
   });
+
+
+  const PieChartData = data =>{
+    return data.map(item=> {
+      return {name: item.status, value: item.count}
+    })
+  }
+
   return (
     <div>
       <h1>Admin Dashboard</h1>
@@ -32,11 +41,37 @@ const AdminDashboardHome = () => {
                 ></path>
               </svg>
             </div>
-            <div className="stat-title font-semibold text-lg">{stat._id}</div>
+            <div className="stat-title font-semibold text-lg">{stat.status}</div>
             <div className="stat-value">{stat.count}</div>
             <div className="stat-desc">Jan 1st - Feb 1st</div>
           </div>
         ))}
+      </div>
+      <div className="w-full h-[400px]">
+        <PieChart className="mx-auto"
+          style={{
+            width: "100%",
+            maxWidth: "500px",
+            maxHeight: "80vh",
+            aspectRatio: 2,
+          }}
+          responsive
+        >
+          <Pie
+            dataKey="value"
+            startAngle={180}
+            endAngle={0}
+            data={PieChartData(stats)}
+            cx="50%"
+            cy="100%"
+            outerRadius="120%"
+            fill="#8884d8"
+            label
+            isAnimationActive={true}
+          />
+        <Legend/>
+        <Tooltip/>
+        </PieChart>
       </div>
     </div>
   );
